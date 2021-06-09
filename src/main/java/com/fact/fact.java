fab0352ae1edce0364fe60310e7b098623042e5d
0309 <pravallika1604@gmail.com>
2021-06-09 14:41:10 +0530

    modify

---
+++ src/main/java/com/fact/fact.java
@@ -0,0 +1,10 @@
+package com.fact;
+public class fact {
+    public static void main(String[] args){
+        int f=10,fac=1;
+        for(int i=f;i>0;i--){
+             fac=fac*i;
+        }
+        System.out.println(fac);
+    }
+}
